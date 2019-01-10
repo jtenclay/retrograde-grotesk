d383b4bbe3d81d334dcf1a27a273459481d48dd6
Jackson Tenclay <jptenclay@gmail.com>
2019-01-10 14:41:24 -0600

    Render text in three.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,27 +1,49 @@
 import '../styles/main.scss';
-
 import * as THREE from 'three';
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+// Scene setup
 
-var renderer = new THREE.WebGLRenderer();
+const scene = new THREE.Scene();
+
+// Camera
+
+let aspect = window.innerWidth / window.innerHeight;
+const d = 20;
+const camera = new THREE.OrthographicCamera( - d * aspect, d * aspect, d, - d, 1, 1000 );
+// camera.position.x = 225;
+// camera.position.y = 200;
+// camera.position.z = 200;
+// camera.lookAt(25, 0, 0);
+
+camera.position.x = 25;
+camera.position.y = 0;
+camera.position.z = 100;
+camera.lookAt(25, 0, 0);
+
+// Renderer
+
+const renderer = new THREE.WebGLRenderer( { alpha: true, antialias: true } );
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-// scene.add( cube );
+// Cube
 
-var loader = new THREE.FontLoader();
+const cubeGeometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( cubeGeometry, material );
 
-loader.load( '/data/gentilis_regular.typeface.json', function ( font ) {
+scene.add( cube );
 
-  var geometry = new THREE.TextGeometry( 'Hello three.js!', {
+// Text
+
+const loader = new THREE.FontLoader();
+
+loader.load( '/data/Retrograde_Grotesk_Regular.json', function ( font ) {
+  // Note: would using a bitmap font fix this messed-up kerning?
+  const textGeometry = new THREE.TextGeometry( 'SWXEDCRFVTGBYHNUJMIKNUHYBGTVFRCD!', {
     font: font,
-    size: 80,
-    height: 5,
+    size: 10,
+    height: 4,
     curveSegments: 12,
     // bevelEnabled: true,
     bevelThickness: 10,
@@ -29,36 +51,45 @@ loader.load( '/data/gentilis_regular.typeface.json', function ( font ) {
     bevelSegments: 5
   } );
 
-  const textMaterials = [
-    new THREE.MeshPhongMaterial({ color: 0xffffff, flatShading: true }),
-    new THREE.MeshPhongMaterial({ color: 0xffffff }),
-   ];
+  const textMaterial = new THREE.MeshPhongMaterial( { color: 0xffffff } );
 
-  var mesh = new THREE.Mesh( geometry, textMaterials );
-  // mesh.position.set( x, y, z );
+  // const textMaterials = [
+  //   new THREE.MeshPhongMaterial({ color: 0xffffff }),
+  //   new THREE.MeshPhongMaterial({ color: 0xffffff }),
+  //  ];
 
-  scene.add( mesh );
+  var textMesh = new THREE.Mesh( textGeometry, textMaterial );
+  textMesh.position.set( 0, 0, 0 );
+
+  scene.add( textMesh );
 } );
 
-// var text = new THREE.TextGeometry( 'hi', loader );
-
-
+// Lights
 
 const pointLight = new THREE.PointLight(0xffffff, 1.5);
 pointLight.position.set(0, 100, 90);
-scene.add(pointLight);
-pointLight.color.setHSL(0.5, 1, 0.5);
+pointLight.color.setHSL(0.25, 1, 0.5);
 
-camera.position.x = 100;
-camera.position.y = 100;
-camera.position.z = 300;
-camera.lookAt(100, 0, 0);
+scene.add(pointLight);
+
+scene.add( new THREE.AmbientLight( 0x333333 ) );
+
+// Animation
 
 function animate() {
   requestAnimationFrame( animate );
 
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
+  cube.position.x += 0.25;
+
+  // Try transitioning from ortho to iso view
+  if (camera.position.x < 225) {
+    camera.position.y += 1;
+    camera.position.x += 1;
+    camera.position.z += .5;
+    camera.lookAt(25, 0, 0);
+  }
   renderer.render( scene, camera );
 }
 animate();
